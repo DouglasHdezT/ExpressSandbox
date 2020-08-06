774dcc94fb00e1182e3254b4fb7bf80424490235
DouglasHdezT <00004516@uca.edu.sv>
2020-08-05 19:46:19 -0600

    Add: User Model

---
+++ ceii_feed/models/User.js
@@ -0,0 +1,55 @@
+const Crypto = require("crypto");
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	hashedPassword: {
+		type: String,
+		default: ""
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	photo: String,
+	posts: {
+		type: [{
+			type: mongoose.Schema.Types.ObjectId,
+			rel: "Post"
+		}]
+	},
+	savedPost: {
+		type: [{
+			type: mongoose.Schema.Types.ObjectId,
+			rel: "Post"
+		}]
+	},
+}, {
+	timestamps: true
+});
+
+UserSchema
+	.virtual("password")
+	.set(function (password) { 
+		this.hashedPassword = Crypto.createHmac("sha256", password).digest("hex");
+	})
+
+UserSchema.methods = {
+	comparePassword: function (password) { 
+		return (
+			Crypto.createHmac("sha256", password).digest("hex") === this.hashedPassword
+		);
+	}
+}	
+module.exports = mongoose.model("User", UserSchema);
