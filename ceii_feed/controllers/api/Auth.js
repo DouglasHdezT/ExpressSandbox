4fd1ca30ec7c151a8641519bfbbcc2a8aad2c5f2
petrlr14 <pedrodeveloper14@gmail.com>
2020-08-05 20:34:58 -0600

    add: register user service and controller

--- ceii_feed/controllers/api/Auth.js
+++ ceii_feed/controllers/api/Auth.js
@@ -1,5 +1,32 @@
+const UserService = require("./../../services/User");
+
 const controller = {}
 
-controller.register = async(req, res) => { }
+controller.register = async (req, res) => { 
+    const fieldValidation = UserService.verifyRegisterFields(req.body);
+    if (!fieldValidation.success) { 
+        return res.status(400).json(fieldValidation.content)
+    }
+    try {
+        const { username, email } = req.body;
+        const userExists = await UserService.findOneUsernameEmail(username, email);
+        
+        if (userExists.success) { 
+            return res.status(409).json({
+                error: "User already exists"
+            });
+        }
+
+        const userRegistered = await UserService.register(req.body)
+        if (!userRegistered.success) { 
+            return res.status(409).json(userRegistered.content);
+        }
+        return res.status(201).json(userRegistered.content);
+    } catch (e) { 
+        return res.status(500).json({
+            error: "Internal Server Error"
+        })
+    }
+}
 
 module.exports = controller;
